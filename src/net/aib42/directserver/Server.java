5736da59a5484819ca23bbecc605b250fcfa84c9
aib <aibok42@gmail.com>
2011-04-05 15:41:05 +0300

    Server now creates clients and manages their ids.

--- src/net/aib42/directserver/Server.java
+++ src/net/aib42/directserver/Server.java
@@ -1,6 +1,8 @@
 package net.aib42.directserver;
 
+import java.io.IOException;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.atomic.AtomicLong;
 
 import net.aib42.directserver.CommandParser;
 import net.aib42.directserver.module.Module;
@@ -9,16 +11,26 @@ public class Server
 {
 	public static final String VERSION_STR = "0.0.1";
 
+	private AtomicLong nextClientId;
 	private CommandParser commandParser;
 
 	public Server()
 	{
+		nextClientId = new AtomicLong();
 		commandParser = new CommandParser(this);
 	}
 
 	public void handleClientConnect(SocketChannel clientChannel)
 	{
-		System.out.println("New client: " + clientChannel.socket().getInetAddress().getHostAddress()); //TODO
+		Client client;
+
+		try {
+			client = new Client(this, nextClientId.incrementAndGet(), clientChannel);
+		} catch (IOException ioe) {
+			System.err.println("Error creating new client:");
+			ioe.printStackTrace();
+			return;
+		}
 	}
 
 	public Module getModuleByCommandPrefix(byte commandPrefix)

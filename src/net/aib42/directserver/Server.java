5ef3a6fcbb69917e69ffb1aa327c3ad430b52d35
aib <aibok42@gmail.com>
2011-04-05 15:34:07 +0300

    Added a CommandParser class for parsing client commands.

--- src/net/aib42/directserver/Server.java
+++ src/net/aib42/directserver/Server.java
@@ -2,10 +2,33 @@ package net.aib42.directserver;
 
 import java.nio.channels.SocketChannel;
 
+import net.aib42.directserver.CommandParser;
+import net.aib42.directserver.module.Module;
+
 public class Server
 {
+	public static final String VERSION_STR = "0.0.1";
+
+	private CommandParser commandParser;
+
+	public Server()
+	{
+		commandParser = new CommandParser(this);
+	}
+
 	public void handleClientConnect(SocketChannel clientChannel)
 	{
 		System.out.println("New client: " + clientChannel.socket().getInetAddress().getHostAddress()); //TODO
 	}
+
+	public Module getModuleByCommandPrefix(byte commandPrefix)
+	{
+		return null;
+	}
+
+	public CommandParser getCommandParser()
+	{
+		return commandParser;
+	}
+
 }
